24424bdcfd8135557652b38bdf1a56912283cc1a
leedag <155880368+leedag@users.noreply.github.com>
2024-06-15 00:03:38 +0900

    Add files via upload

---
+++ calculator/calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+
+	if ((A >= 1) && (A <= 10000) && (B >= 1) && (B <= 10000)) {
+		cout << A + B << endl;
+		cout << A - B << endl;
+		cout << A * B << endl;
+		cout << A / B << endl;
+		cout << A % B << endl;
+	}
+	return 0;
+}
